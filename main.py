42ababc19953ef7c6f9bf40474f34c6466a791c2
Yanush Viktar <vicjan89@gmail.com>
2021-09-18 10:11:02 +0300

    Рабочая версия сравнения в выдачей в 3 файла: 1. литература совпадает в обоих списках; 2. Литература только в списке 1; 3. Литература только в списке 2.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import Levenshtein
 list1 = []
+f = True
+both = open('both.txt', 'w', encoding='utf8')
+only1 = open('only1.txt', 'w', encoding='utf8')
+only2 = open('only2.txt', 'w', encoding='utf8')
 with open("Список СРЗАИ.txt", "r", encoding='utf8') as file1:
     for line in file1:
         t = line.find('\t')
@@ -7,7 +11,19 @@ with open("Список СРЗАИ.txt", "r", encoding='utf8') as file1:
         list1.append(line)
 with open("Список ПТО.txt", "r", encoding='utf8') as file2:
     list2 = file2.readlines()
+    c2 = list(range(len(list2)))
 for lit1 in list1:
-    for lit2 in list2:
+    f = True
+    for l2, lit2 in enumerate(list2):
         if Levenshtein.ratio(lit1, lit2)>0.8:
-            print(lit1[:-1],lit2[:-1])
+            both.write(lit1+' : '+lit2)
+            f = False
+            if l2 in c2:
+                c2.remove(l2)
+    if f:
+        only1.write(lit1)
+for s in c2:
+    only2.write(list2[s])
+both.close()
+only1.close()
+only2.close()
