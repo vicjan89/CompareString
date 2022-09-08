86dc03af28da680388f8e0d690e30efb127179d7
Yanush Viktar <vicjan89@gmail.com>
2022-09-08 09:24:02 +0300

    Добавлен README.md и выверен список СРЗАИ.txt

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import Levenshtein
 list1 = []
 f = True
-both = open('both.txt', 'w', encoding='utf8')
-only1 = open('only1.txt', 'w', encoding='utf8')
-only2 = open('only2.txt', 'w', encoding='utf8')
-with open("Список СРЗАИ.txt", "r", encoding='utf8') as file1:
-    for line in file1:
-        t = line.find('\t')
-        line = line[t+1:]
-        list1.append(line)
-with open("Список ПТО.txt", "r", encoding='utf8') as file2:
-    list2 = file2.readlines()
-    c2 = list(range(len(list2)))
-for lit1 in list1:
-    f = True
-    for l2, lit2 in enumerate(list2):
-        if Levenshtein.ratio(lit1, lit2)>0.7:
-            both.write(lit1+' : '+lit2)
-            f = False
-            if l2 in c2:
-                c2.remove(l2)
-    if f:
-        only1.write(lit1)
-for s in c2:
-    only2.write(list2[s])
+both = open('В обоих списках.txt', 'w', encoding='utf8')
+only2 = open('Есть во 2 но нет в 1.txt', 'w', encoding='utf8')
+with open("Список для сравнения", "r", encoding='utf8') as file1:
+    list2 = file1.readlines()
+with open("Список СРЗАИ.txt", "r", encoding='utf8') as file2:
+    list1 = []
+    for row in file2:
+        if '#электромонтёр' in row:
+            list1.append(row.replace('#электромонтёр', ''))
+
+list1.sort()
+for row in list1:
+    only2.write(row)
+
+# result_comparison = [[0.0 for i in range(len(list1))] for j in range(len(list2))]
+# for i, literature1 in enumerate(list1):
+#     for j, literature2 in enumerate(list2):
+#         result_comparison[j][i] = Levenshtein.ratio(literature1, literature2)
+# for j, result in enumerate(result_comparison):
+#     max_result = max(result)
+#     if max_result > 0.7:
+#         both.write(list2[j] + ' : ' + list1[result.index(max_result)])
+#     else:
+#         only2.write(list2[j])
 both.close()
-only1.close()
 only2.close()
