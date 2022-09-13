e921f31e714a7c48162ad70426724b6f31bc9c66
Yanush Viktar <vicjan89@gmail.com>
2022-09-13 17:05:14 +0300

    Переведена функциональность в класс, добавлена возможность фильтрации списка и его сортировки

--- main.py
+++ main.py
@@ -1,29 +1,69 @@
 import Levenshtein
-list1 = []
-f = True
-both = open('В обоих списках.txt', 'w', encoding='utf8')
-only2 = open('Есть во 2 но нет в 1.txt', 'w', encoding='utf8')
-with open("Список для сравнения", "r", encoding='utf8') as file1:
-    list2 = file1.readlines()
-with open("Список СРЗАИ.txt", "r", encoding='utf8') as file2:
-    list1 = []
-    for row in file2:
-        if '#электромонтёр' in row:
-            list1.append(row.replace('#электромонтёр', ''))
 
-list1.sort()
-for row in list1:
-    only2.write(row)
+class CompareLiterature:
 
-# result_comparison = [[0.0 for i in range(len(list1))] for j in range(len(list2))]
-# for i, literature1 in enumerate(list1):
-#     for j, literature2 in enumerate(list2):
-#         result_comparison[j][i] = Levenshtein.ratio(literature1, literature2)
-# for j, result in enumerate(result_comparison):
-#     max_result = max(result)
-#     if max_result > 0.7:
-#         both.write(list2[j] + ' : ' + list1[result.index(max_result)])
-#     else:
-#         only2.write(list2[j])
-both.close()
-only2.close()
+    def __init__(self, list_literature1: str = '', list_literature2: str=''):
+        self.list_literature1 = list_literature1
+        self.list_literature2 = list_literature2
+
+    @staticmethod
+    def load(name_file, filter=''):
+        out_list = []
+        with open(name_file, "r", encoding='utf8') as file:
+            for line in file:
+                if filter in line:
+                    out_list.append(line.split('#')[0])
+        return out_list
+
+    def load_first(self, list_literature: str = '', filter=''):
+        if list_literature:
+            self.list_literature1 = list_literature
+        self.list1 = self.load(self.list_literature1, filter)
+
+    def load_second(self, list_literature: str = '', filter=''):
+        if list_literature:
+            self.list_literature2 = list_literature
+        self.list2 = self.load(self.list_literature2, filter)
+
+    def compare(self):
+        self.result_comparison = [[0.0 for i in range(len(self.list1))] for j in range(len(self.list2))]
+        for i, literature1 in enumerate(self.list1):
+            for j, literature2 in enumerate(self.list2):
+                self.result_comparison[j][i] = Levenshtein.ratio(literature1, literature2)
+
+    def separate(self, ratio):
+        self.list_both = []
+        self.list_only2 = []
+        for j, result in enumerate(self.result_comparison):
+            max_result = max(result)
+            if max_result > ratio:
+                self.list_both.append(self.list2[j] + ' : ' + self.list1[result.index(max_result)] + '\n')
+            else:
+                self.list_only2.append(self.list2[j])
+
+    @staticmethod
+    def save(name_file, list_str):
+        with open(name_file, 'w', encoding='utf-8') as file:
+            for row in list_str:
+                file.write(row + '\n')
+
+    def save_both(self, name_file):
+        self.save(name_file, self.list_both)
+
+    def save_only2(self, name_file):
+        self.save(name_file, self.list_only2)
+
+    def save_first(self, name_file):
+        self.save(name_file, sorted(self.list1))
+
+
+
+
+cl = CompareLiterature()
+cl.load_first(list_literature="Список СРЗАИ.txt", filter='техник_метролог')
+# cl.load_second(list_literature="Список для сравнения")
+# cl.compare()
+# cl.separate(0.5)
+# cl.save_both('В обоих списках.txt')
+# cl.save_only2('Есть во 2 но нет в 1.txt')
+cl.save_first('В должностную.txt')
